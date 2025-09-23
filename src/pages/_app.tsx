099867911403a2fefbc29c55a2133d568b9ceb5d
jheremois <jheremy802@gmail.com>
2025-09-23 14:25:46 -0400

    add analitics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,40 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+// Google Analytics page view tracking
+const GA_TRACKING_ID = "G-6C1H3SCXKM";
+
+declare global {
+  interface Window {
+    gtag: (...args: any[]) => void;
+  }
+}
+
+// Track page views
+const pageview = (url: string) => {
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
+  }
+};
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6C1H3SCXKM"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-6C1H3SCXKM');
+            `,
+          }}
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
